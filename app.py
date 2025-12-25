ee780d59315366fd3597400a208b4e539db81e26
yeswanth <yeswanthbehara1111@gmail.com>
2025-12-25 11:36:08 +0000

    added the source codes in main branch

---
+++ app.py
@@ -0,0 +1,6 @@
+def greet(name):
+    return f"Hello, {name}! Welcome to DevOps from main branch."
+
+if __name__ == "__main__":
+    user = "Yeswanth"
+    print(greet(user))
---
+++ test_app.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Add:", add(10, 5))
+    print("Subtract:", subtract(10, 5))
